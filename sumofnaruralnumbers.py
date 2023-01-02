49579b0224725b616870b93e238a8a0c5b6b3505
Boomika-M <boomika.mahendhran@gmail.com>
2023-01-02 07:22:29 +0530

    sum of n natural numbers

--- sumofnaruralnumbers.py
+++ sumofnaruralnumbers.py
@@ -2,7 +2,7 @@ num=int(input("Enter the number:"))
 while num<=0:
     num=int(input("Enter the positive number:"))
 else:
-    sum=0:
+    sum=0
     for i in range(1,num+1):
         sum=sum+i
-print("Sum oof",num,"natural numbers is",sum)
+print("Sum of",num,"natural numbers is",sum) 
